44181bacfdc29557a2112e3fec8f7d7a54e05211
BlaSSSTTT <109813312+BlaSSSTTT@users.noreply.github.com>
2023-12-30 17:38:18 +0000

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,27 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Main {
 
     public static void main(String[] args) throws TelegramApiException {
+        Runnable myRunnable = new MyRunnable();
+        Thread myThread = new Thread(myRunnable);
+        myThread.start();
+
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
             telegramBotsApi.registerBot(new WeatherBot());
         } catch (TelegramApiRequestException e) {
             System.out.println("Не вдалося запустити бота");
+        }
+    }
+   static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
